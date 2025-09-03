6dbcb327b4bdcce5d32a4c1d0f48ef1df873863d
tf <kaersagen@hotmail.com>
2025-09-03 21:27:45 +0800

    修改flake.nix的缩进

--- flake.nix
+++ flake.nix
@@ -6,64 +6,62 @@
   };
 
   outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+    };
+  in
+  {
+    packages = forAllSystems (system:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: import nixpkgs {
-        inherit system;
-      };
+      pkgs = pkgsFor system;
     in
     {
-      packages = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "notify";
-            version = "0.1.0";
-            src = ./.;
-            
-            cargoLock = { lockFile = ./Cargo.lock; };
-            
-            nativeBuildInputs = [
-              pkgs.pkg-config
-            ];
-            
-            buildInputs = [
-              pkgs.dbus
-            ];
-            
-            # Add runtime dependencies
-            propagatedBuildInputs = [
-              pkgs.python3Packages.speedtest-cli
-            ];
-            
-            meta = with pkgs.lib; {
-              description = "A Rust application that monitors bandwidth and sends notifications";
-              homepage = "https://github.com/user/notify";
-              license = licenses.mit;
-              mainProgram = "notify";
-            };
-          };
-        }
-      );
-      
-      devShells = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.rustc
-              pkgs.cargo
-              pkgs.pkg-config
-              pkgs.dbus
-              pkgs.python3Packages.speedtest-cli
-            ];
-          };
-        }
-      );
-    };
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "notify";
+        version = "0.1.0";
+        src = ./.;
+        
+        cargoLock = { lockFile = ./Cargo.lock; };
+        
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+        
+        buildInputs = [
+          pkgs.dbus
+        ];
+        
+        # Add runtime dependencies
+        propagatedBuildInputs = [
+          pkgs.python3Packages.speedtest-cli
+        ];
+        
+        meta = with pkgs.lib; {
+          description = "A Rust application that monitors bandwidth and sends notifications";
+          homepage = "https://github.com/user/notify";
+          license = licenses.mit;
+          mainProgram = "notify";
+        };
+      };
+    });
+    
+    devShells = forAllSystems (system:
+    let
+      pkgs = pkgsFor system;
+    in
+    {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.pkg-config
+          pkgs.dbus
+          pkgs.python3Packages.speedtest-cli
+        ];
+      };
+    });
+  };
 }
